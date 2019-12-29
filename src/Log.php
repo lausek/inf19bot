38c88f43c1bfb4979f9d51838a18f4e87bf239bd
lausek <root@lausek.eu>
2019-12-29 23:34:02 +0100

    logging refactoring

--- src/Log.php
+++ src/Log.php
@@ -2,14 +2,52 @@
 
 class Log
 {
-    static function trace($msg)
+    public const INFO = 'INFO';
+    public const WARN = 'WARN';
+    public const ERROR = 'ERROR';
+
+    static function format($msg, $level)
+    {
+        $stamp = date('Y-m-d h:i:s');
+        return "[$stamp][$level] $msg";
+    }
+
+    static function write($msg)
     {
         $location = Util::get_config('tracefile');
         if (null !== $location)
         {
             $fname = Util::path($location);
-            $stamp = date('Y-m-d h:i:s');
-            file_put_contents($fname, "[$stamp] $msg\n", FILE_APPEND);
+            file_put_contents($fname, $msg . "\n", FILE_APPEND);
         }
+    }
+
+    static function trace($msg)
+    {
+        Log::write(Log::format($msg, Log::INFO));
+    }
+
+    static function etrace($msg)
+    {
+        static $ids = null;
+
+        if (null === $ids)
+        {
+            $cache = new Cache('Ids');
+            $ids = isset($cache->forward_err_to) ? $cache->forward_err_to : [];
+        }
+
+        $formatted = Log::format($msg, Log::ERROR);
+
+        if (!empty($ids))
+        {
+            $client = Util::get_client();
+            foreach ($cache->forward_err_to as $forward_to)
+            {
+                $client->sendMessage($forward_to, $formatted, 'markdown');
+            }
+        }
+
+        Log::write($formatted);
     }
 }

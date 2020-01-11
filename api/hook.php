cf4d8be94e94701e1701015de4059fafb9f5eef7
lausek <root@lausek.eu>
2020-01-11 16:58:01 +0100

    refactor api/hook.php

--- api/hook.php
+++ api/hook.php
@@ -30,6 +30,45 @@ function handle_cmd_output($client, $output)
     $client->sendMessage($chat_id, $output, 'markdown');
 }
 
+function handle_callback($client, $update)
+{
+    $cache = new Cache('Callback');
+    $cmid = ChatMessageId::from(
+        $update->callback_query->message->chat->id,
+        $update->callback_query->message->message_id
+    );
+    $id = (string) $cmid;
+
+    if (isset($cache[$id]))
+    {
+        $cmd = new $cache[$id]();
+        $cmd->callback_on($cmid, $update);
+    }
+    else
+    {
+        Log::trace("$id is not in `Callback` cache");
+    }
+
+    $client->answerCallbackQuery($update->callback_query->id);
+}
+
+function handle_message($client, $update)
+{
+    if ('group' === $update->message->chat->type)
+    {
+        if (null === Cache::get_nerds_id())
+        {
+            Cache::set_nerds_id($client->easy->chat_id);
+        }
+    }
+
+    $cmd = CommandParser::process($update->message->text);
+    if (false !== $cmd)
+    {
+        handle_cmd_output($client, $cmd->run($update));
+    }
+}
+
 function main()
 {
     $client = Util::get_client();
@@ -37,43 +76,13 @@ function main()
     
     Log::trace(var_export($update, true));
     
-    // TODO: refactor
     if (isset($update->callback_query))
     {
-        $cache = new Cache('Callback');
-        $cmid = ChatMessageId::from(
-            $update->callback_query->message->chat->id,
-            $update->callback_query->message->message_id
-        );
-        $id = (string) $cmid;
-
-        if (isset($cache[$id]))
-        {
-            $cmd = new $cache[$id]();
-            $cmd->callback_on($cmid, $update);
-        }
-        else
-        {
-            Log::trace("$id is not in `Callback` cache");
-        }
-        
-        $client->answerCallbackQuery($update->callback_query->id);
+        handle_callback($client, $update);
     }
     else if (isset($update->message))
     {
-        if ('group' === $update->message->chat->type)
-        {
-            if (null === Cache::get_nerds_id())
-            {
-                Cache::set_nerds_id($client->easy->chat_id);
-            }
-        }
-    
-        $cmd = CommandParser::process($update->message->text);
-        if (false !== $cmd)
-        {
-            handle_cmd_output($client, $cmd->run($update));
-        }
+        handle_message($client, $update);
     }
 }
 

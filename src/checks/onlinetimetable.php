f3f4d49242a1d660b2b821c5c33d211a5725ae28
lausek <root@lausek.eu>
2020-10-23 15:21:56 +0200

    add support for discord webhook

--- src/checks/onlinetimetable.php
+++ src/checks/onlinetimetable.php
@@ -21,7 +21,6 @@ class OnlinetimetableCheck extends Check
             $dt_last_update = new DateTime($this->cache['last_update']);
         }
 
-
         // is it after 18 o'clock
         $late_enough = 18 <= intval($dt_today->format('H'));
 
@@ -97,16 +96,70 @@ class OnlinetimetableCheck extends Check
                     {
                         $msg .= "- $module\n";
                     }
-                    $response->add_message($msg);
 
-                    // if this process was requested from the nerds chat, don't emit it twice 
-                    if (!$response->is_nerds()) {
-                        Util::inform_nerds($msg);
-                    }
+                    $this->post_calendar($response, $msg);
                 }
             }
 
             $this->cache['last_update'] = $today;
+        }
+    }
+
+    public function post_calendar($reponse, $msg)
+    {
+        $response->add_message($msg);
+
+        // if this process was requested from the nerds chat, don't emit it twice 
+        if (!$response->is_nerds())
+        {
+            Util::inform_nerds($msg);
+        }
+
+        $this->try_discord_webhook_forward($msg);
+    }
+
+    public function try_discord_webhook_forward($msg)
+    {
+        $webhook = Util::get_config('online_timetable_discord_webhook');
+        if (null !== $webhook)
+        {
+            $creds = $this->get_discord_webhook_info($webhook);
+            if (null === $creds)
+            {
+                Log::etrace('couldn\'t retrieve discord webhook credentials');
+            }
+
+            $this->post_discord_webhook_msg($creds, $msg);
+        }
+    }
+
+    private function get_discord_webhook_info($url)
+    {
+        $content = file_get_contents($url, false);
+        if (false === $content)
+        {
+            return null;
+        }
+        return json_decode($content);
+    }
+
+    private function post_discord_webhook_msg($creds, $msg)
+    {
+        $url = "https://discord.com/api/webhooks/$creds->id/$creds->token";
+        $payload = ['content' => $msg];
+
+        $req = \curl_init();
+        curl_setopt($req, CURLOPT_URL, $url);
+        curl_setopt($req, CURLOPT_POST, true);
+        curl_setopt($req, CURLOPT_POSTFIELDS, $payload);
+
+        $head = curl_exec($req);
+        $status = curl_getinfo($req, CURLINFO_HTTP_CODE);;
+        curl_close($req);
+
+        if (200 != $status)
+        {
+            Log::etrace('discord webhook post failed');
         }
     }
 }
